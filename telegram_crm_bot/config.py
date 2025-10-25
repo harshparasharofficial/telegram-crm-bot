519eb572b46fc066bcead0ab06b8d9efb9be3619
HarshParashar.Official <harshparashar.official@gmail.com>
2025-10-25 17:59:49 +0530

    Update config.py

--- telegram_crm_bot/config.py
+++ telegram_crm_bot/config.py
@@ -1,9 +1,2 @@
-import os
-
-BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Telegram Bot token
-ADMIN_IDS = [int(x) for x in os.environ.get("ADMIN_IDS", "").split(",") if x]  # comma separated Telegram IDs
-DEFAULT_LANGUAGE = os.environ.get("DEFAULT_LANGUAGE", "en")  # en / hi / mix
-
-# Firebase credentials will be passed as JSON string in env var FIREBASE_CREDENTIALS
-FIREBASE_CREDENTIALS = os.environ.get("FIREBASE_CREDENTIALS")
-FIREBASE_DB_URL = os.environ.get("FIREBASE_DB_URL")  # e.g. https://your-project-default-rtdb.firebaseio.com
+BOT_TOKEN = "8346975704:AAGH5vTTKXgYTSZxSWoWp6AtBT8-83RlY4Y"
+SHEET_NAME = "LeadPilot_CRM_Data"
